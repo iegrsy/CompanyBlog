3864273b68731f92c2c32e4580bb303b7eecf55d
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2018-12-04 17:53:23 +0300

    Add login activity.

--- CompanyBlogMobile/app/src/main/java/space/iegrsy/companyblogmobile/SplashActivity.java
+++ CompanyBlogMobile/app/src/main/java/space/iegrsy/companyblogmobile/SplashActivity.java
@@ -1,13 +1,35 @@
 package space.iegrsy.companyblogmobile;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Activity context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                context.finish();
+            }
+        }).start();
     }
 }
