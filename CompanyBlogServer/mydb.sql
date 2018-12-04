6b6227e882b0fd86686d6f78f9080f8cb1e5a66b
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2018-12-04 09:51:54 +0300

    Update db types.

--- CompanyBlogServer/mydb.sql
+++ CompanyBlogServer/mydb.sql
@@ -1,121 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
---
--- Anamakine: localhost
--- Üretim Zamanı: 03 Ara 2018, 18:16:21
--- Sunucu sürümü: 5.7.24-0ubuntu0.16.04.1
--- PHP Sürümü: 7.0.32-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Veritabanı: `mydb`
---
-CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mydb`;
-
--- --------------------------------------------------------
-
---
--- Tablo için tablo yapısı `Comments`
---
-
-DROP TABLE IF EXISTS `Comments`;
-CREATE TABLE `Comments` (
-  `id` int(11) NOT NULL,
-  `postid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `comment` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Tablo için tablo yapısı `Posts`
---
-
-DROP TABLE IF EXISTS `Posts`;
-CREATE TABLE `Posts` (
-  `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `body` varchar(5000) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Tablo için tablo yapısı `Users`
---
+CREATE DATABASE IF NOT EXISTS "mydb" DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE mydb;
 
 DROP TABLE IF EXISTS `Users`;
-CREATE TABLE `Users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `birthday` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE "Users" (
+  "id" int(11) NOT NULL AUTO_INCREMENT,
+  "username" varchar(50) NOT NULL,
+  "email" varchar(50) NOT NULL,
+  "password" varchar(50) NOT NULL,
+  "birthday" date NOT NULL,
+  PRIMARY KEY (id)
+);
 
---
--- Dökümü yapılmış tablolar için indeksler
---
+DROP TABLE IF EXISTS `Posts`;
+CREATE TABLE "Posts" (
+  "id" int(11) NOT NULL AUTO_INCREMENT,
+  "userid" int(11) NOT NULL,
+  "title" varchar(100) NOT NULL,
+  "body" varchar(5000) NOT NULL,
+  "date" datetime NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userid) REFERENCES Users(id)
+);
 
---
--- Tablo için indeksler `Comments`
---
-ALTER TABLE `Comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `postid` (`postid`),
-  ADD KEY `userid` (`userid`);
+DROP TABLE IF EXISTS `Comments`;
+CREATE TABLE "Comments" (
+  "id" int(11) NOT NULL AUTO_INCREMENT,
+  "postid" int(11) NOT NULL,
+  "userid" int(11) NOT NULL,
+  "comment" varchar(500) NOT NULL,
+  "date" datetime NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userid) REFERENCES Users(id),
+  FOREIGN KEY (postid) REFERENCES Posts(id)
+);
 
---
--- Tablo için indeksler `Posts`
---
-ALTER TABLE `Posts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `userid` (`userid`);
+INSERT INTO `Users` (`id`, `username`, `email`, `password`, `birthday`) VALUES
+(1, 'ieg', 'ieg@gmail.com', 'f447b20a7fcbf53a5d5be013ea0b15af', '1995-06-01'),
+(2, 'asd', 'asd@gmail.com', 'f447b20a7fcbf53a5d5be013ea0b15af', '1995-12-04');
 
---
--- Tablo için indeksler `Users`
---
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `Posts` (`id`, `userid`, `title`, `body`, `date`) VALUES
+(1, 1, 'merhaba', 'ilk yazim', '2018-11-01 05:14:14');
 
---
--- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
---
+INSERT INTO `Comments` (`id`, `postid`, `userid`, `comment`, `date`) VALUES
+(1, 1, 2, 'güzel', '2018-11-03 05:18:14');
 
---
--- Tablo için AUTO_INCREMENT değeri `Users`
---
-ALTER TABLE `Users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Dökümü yapılmış tablolar için kısıtlamalar
---
-
---
--- Tablo kısıtlamaları `Comments`
---
-ALTER TABLE `Comments`
-  ADD CONSTRAINT `Comments_ibfk_1` FOREIGN KEY (`postid`) REFERENCES `Posts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `Comments_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `Users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Tablo kısıtlamaları `Posts`
---
-ALTER TABLE `Posts`
-  ADD CONSTRAINT `Posts_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
