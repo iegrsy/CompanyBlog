126a933c9c5e80889ebcffe953e34b0952137133
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2018-12-06 17:52:56 +0300

    Add Home pages.

--- CompanyBlogMobile/app/src/main/java/space/iegrsy/companyblogmobile/MainActivity.java
+++ CompanyBlogMobile/app/src/main/java/space/iegrsy/companyblogmobile/MainActivity.java
@@ -3,28 +3,38 @@ package space.iegrsy.companyblogmobile;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    private TextView mTextMessage;
+    @BindView(R.id.home_container)
+    FrameLayout container;
+
+    @BindView(R.id.home_navigation)
+    BottomNavigationView navigation;
+
+    private FragmentManager fragmentManager;
+    private HomeFragment homeFragment;
+    private ProfileFragment profileFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    switchFragment(homeFragment);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    switchFragment(profileFragment);
                     return true;
             }
             return false;
@@ -36,9 +46,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        ButterKnife.bind(this);
+        initUI();
     }
 
+    private void initUI() {
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+
+        fragmentManager = getSupportFragmentManager();
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_home);
+    }
+
+    private void switchFragment(Fragment fragment) {
+        if (fragment != null)
+            fragmentManager.beginTransaction().replace(R.id.home_container, fragment).commit();
+    }
 }
---
+++ CompanyBlogMobile/app/src/main/java/space/iegrsy/companyblogmobile/PostFragment.java
@@ -0,0 +1,6 @@
+package space.iegrsy.companyblogmobile;
+
+import android.support.v4.app.Fragment;
+
+public class PostFragment extends Fragment{
+}
